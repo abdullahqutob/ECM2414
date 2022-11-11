9a79f218e08abeed1c2740a90b3168c5aec0cd88
sc1131 <sc1131@exeter.ac.uk>
2022-11-11 16:59:13 +0000

    Player Class, intial development

--- src/Player.java
+++ src/Player.java
@@ -1,16 +1,26 @@
 import java.util.ArrayList;
 
-public class Player extends Thread {
+public class Player implements Runnable {
 
     private int playerDenomination;
     private ArrayList<Card> hand = new ArrayList<Card>();
+    private int drawDeck;
+    private int discardedDeck;
 
 
-    public Player(int playerDenomination){
-        this.playerDenomination = playerDenomination;
+    public Player(int number){
+        this.playerDenomination = number;
+        this.drawDeck = number;
+        if(number == CardGame.num) {
+            discardedDeck = 1;
+        }
+        else {
+            this.discardedDeck = number + 1;
+        }
     }
 
+    @Override
+    public void run() {
 
-
-
+    }
 }
