41b3e45d7aaf55a75b5ed2dbad1d2a7a14a3b416
Abdullah Qutob <aboudyqutob@gmail.com>
2022-11-11 14:52:42 +0000

    Card Class

---
+++ src/Card.java
@@ -0,0 +1,12 @@
+public class Card {
+
+    private final int CardNum;
+
+    public Card(int CardNum){
+        this.CardNum = CardNum;
+    }
+
+    public int getCardNum(){
+        return CardNum;
+    }
+}
