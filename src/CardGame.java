95dcf32ac43e6c5ef4ef2af0ea456775686266d7
sc1131 <sc1131@exeter.ac.uk>
2022-11-11 16:31:01 +0000

    Player Class, intial development

--- src/CardGame.java
+++ src/CardGame.java
@@ -1,7 +1,3 @@
 public class CardGame {
-
-
-    public static void main(String[] args) {
-
-    }
+    public static void main(String[] args) {}
 }
--- src/Deck.java
+++ src/Deck.java
@@ -14,12 +14,11 @@ public class Deck {
 
       Card firstCard = this.deck.get(0);
       return firstCard;
-
   }
 
     public int getNumOfCards() {
         return numOfCards;
-    }
+  }
 
     public ArrayList<Card> getDeck() {
         return deck;
