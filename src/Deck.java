f8c8c90bfa805879ef622a74b9f22841fa995ada
sc1131 <sc1131@exeter.ac.uk>
2022-11-11 15:31:48 +0000

    Deck Class

---
+++ src/Deck.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+
+public class Deck {
+
+  ArrayList<Card> deck = new ArrayList<Card>();
+  int numOfCards;
+
+  public Deck(int numOfCards,ArrayList<Card>deck){
+      this.numOfCards=numOfCards;
+      this.deck = deck;
+  }
+
+  public synchronized Card drawCard (Card card){
+
+      Card firstCard = this.deck.get(0);
+      return firstCard;
+
+  }
+
+    public int getNumOfCards() {
+        return numOfCards;
+    }
+
+    public ArrayList<Card> getDeck() {
+        return deck;
+    }
+}
