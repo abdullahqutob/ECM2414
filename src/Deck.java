789c78e8953f2df1c61d8c8df4e95727b2f0145a
Abdullah Qutob <aboudyqutob@gmail.com>
2022-11-12 12:47:52 +0000

    Added insert card function to deck

--- src/Deck.java
+++ src/Deck.java
@@ -11,15 +11,30 @@ public class Deck {
       this.deck = deck;
   }
 
+    /**
+     *
+     * @param card
+     * @return first card from deck
+     */
   public synchronized Card drawCard (Card card){
 
       Card firstCard = this.deck.get(0);
       return firstCard;
   }
 
+    /**
+     * Inserts card into end of deck
+     * @param card
+     */
+    public void insertCard (Card card){
+        this.deck.add(deck.size()-1, card);
+    }
     public int getNumberofDeck() {
         return Number;
   }
+
+
+
 
     public ArrayList<Card> getDeck() {
         return deck;
